3e544c20d808f9afc22988fb25b398b294525fba
tyoo2025 <146300811+tyoo2025@users.noreply.github.com>
2023-10-22 14:01:29 -0700

    Added printing recipe and ratings

--- Recipe/map_ingr.py
+++ Recipe/map_ingr.py
@@ -55,3 +55,63 @@ def get_main_ingr(recipe_title):
             ingr_name_list.append(word)
     print(ingr_name_list, "in title of recipe")
     return get_ingr_ids(ingr_name_list)
+
+def get_avg_rating(recipe_id):
+    """given a recipe_id, pull all of the relevant ratings, and calculate total and avg ratings for the recipe
+        Returns the total number of ratings, avg rating"""
+    #generate dataframe for all the reviews that have a recipe_id matching recipe_id
+    relevant_ratings_df = raw_ratings_df.query('recipe_id == @recipe_id')
+
+    #get list of the rating from the relevant reviews:
+    all_ratings = relevant_ratings_df['rating'].tolist()
+    num_ratings = len(all_ratings)
+    avg_rating = sum(all_ratings)/num_ratings
+    return num_ratings, avg_rating
+
+def order_ratings(recipe_pool):
+    """Given a list of recipe IDs in our pool, 
+        return an ordered list of recipe ids (highest -> lowest weighted rating)"""
+    recipe_pool_dict = {}
+    for id in recipe_pool:
+        total_r, avg_r = get_avg_rating(id)
+        recipe_pool_dict[id] = total_r*avg_r
+
+    recipe_pool_ordered = sorted(recipe_pool_dict, key=recipe_pool_dict.get, reverse=True)
+
+    #We may want to add something here to only return part of the ordered pool of recipes!
+    return recipe_pool_ordered
+
+def display_recipe(recipe_pool):
+    """Given recipe_pool (a list of recipe IDs in our final pool)
+    Display/Print all recipes for the user"""
+    all_recipe_ids = raw_recipes_df['id'].tolist()
+    recipe_counter = 1
+    while recipe_counter <= 5 and recipe_counter<= len(recipe_pool):
+        recipe_id = recipe_pool[recipe_counter-1]
+        recipe_title = map_recipe_id_name(recipe_id)
+        num_rating, avg_rating = get_avg_rating(recipe_id)
+        rating_str = f'Rating: {avg_rating:2f} among {num_rating} users'
+        recipe_ind = all_recipe_ids.index(recipe_id)
+        recipe_row = raw_recipes_df.iloc[recipe_ind]
+        time_est = f'{recipe_row["minutes"]} minutes'
+        ingredients = recipe_row["ingredients"]
+        description = recipe_row["description"]
+        instruction = eval(recipe_row['steps'])
+
+        print(f'Recipe #{recipe_counter}')
+        #print recipe title
+        print(recipe_title)
+        #print ratings, number of ratings
+        print(rating_str)
+        # print time estimate
+        print(time_est)
+        #print ingredients
+        print('Ingredients:', ingredients)
+        #print instructions
+        print("Steps:")
+        for i in range(len(instruction)):
+            print(i, instruction[i])
+        print()
+        recipe_counter += 1
+
+
