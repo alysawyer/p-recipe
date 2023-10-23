648b026f59d907ffa01a67a145bbeb3a85a11018
ayjj2023 <ayjj2023@mymail.pomona.edu>
2023-10-23 15:17:57 -0700

    deleting mistake

--- Recipe/map_ingr.py
+++ Recipe/map_ingr.py
@@ -139,21 +139,8 @@ def get_missing_ingredients(recipe_id, list_of_ingr):
             counter += 1
     return counter 
 
-#recipes sorted by main ingredient
-#key: main ingredient ID, value: list of recipe IDs with that main ingredient
-#a recipe can appear multiple times 
-sorted_recipes = {} 
-for id in ingr_map['id']:
-    sorted_recipes[id] = []
-sorted_recipes[-1] = [] #if a recipe has no main ingredient, add here
+main_ingredients = {}
 for id in raw_recipes_df['id']:
-    recipe_name = map_recipe_id_name(id)
-    print(recipe_name)
-    main_ingredients = get_main_ingr(str(recipe_name))
-    for ingr in main_ingredients:
-        sorted_recipes[ingr].append(id)
-    if not main_ingredients:
-        sorted_recipes[-1].append(id)
+    main_ingredients[id] = get_main_ingr(str(map_recipe_id_name(id)))
 
-print(sorted_recipes[4110])
-#find_candidates
+print(main_ingredients[112140])
