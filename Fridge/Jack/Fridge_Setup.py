d3210fb70f2fc57b870aee3c9ac85388bfaaeb92
JACK_SUSANK <jsusank5@gmail.com>
2023-11-05 14:53:47 -0800

    reorganizing data function

--- Fridge/Jack/Fridge_Setup.py
+++ Fridge/Jack/Fridge_Setup.py
@@ -29,6 +29,7 @@ import locale
 locale.setlocale(locale.LC_ALL, 'en_US')
 
 
+
 # Ignore warnings
 import warnings
 warnings.filterwarnings("ignore")
@@ -68,8 +69,7 @@ new_image_file_path = "/Users/jacksusank/Downloads/p-ai/fridge_data/SmarterChef.
 # old   names = ["Apple", 'Banana', 'Beans', 'Capsicum', 'Carrot', 'Cucumber', 'Curli-Flower', 'Orange', 'Tomato', 'Tomatos', 'apple', 'asparagus', 'avocado', 'banana', 'beef', 'bell_pepper', 'bento', 'blueberries', 'bottle', 'bread', 'broccoli', 'butter', 'can', 'carrot', 'cauliflower', 'cheese', 'chicken', 'chicken_breast', 'chocolate', 'coffee', 'corn', 'cucumber', 'egg', 'eggs', 'energy_drink', 'fish', 'flour', 'garlic', 'goat_cheese', 'grapes', 'grated_cheese', 'green_beans', 'ground_beef', 'guacamole', 'ham', 'heavy_cream', 'humus', 'juice', 'ketchup', 'kothmari', 'leek', 'lemon', 'lettuce', 'lime', 'mango', 'marmelade', 'mayonaise', 'milk', 'mushrooms', 'mustard', 'nuts', 'onion', 'orange', 'pak_choi', 'parsley', 'peach', 'pear', 'pineapple', 'plasticsaveholder', 'pot', 'potato', 'potatoes', 'pudding', 'red_cabbage', 'red_grapes', 'rice_ball', 'salad', 'sandwich', 'sausage', 'shrimp', 'smoothie', 'spinach', 'spring_onion', 'strawberries', 'sugar', 'sweet_potato', 'tea_a', 'tea_i', 'tomato', 'tomato_sauce', 'tortillas', 'turkey', 'watermelon', 'yogurt']
 names = ["Lemon", 'Onion', 'Orange', 'Peas', 'Potato', 'Strawberry', 'Tomato', 'apples', 'aubergine', 'bananas', 'blueberries', 'bread', 'broccoli', 'butter', 'carrots', 'cheese', 'chicken', 'courgettes', 'eggs', 'ginger', 'green beans', 'green chilies', 'ham', 'lemon', 'lettuce', 'lime', 'milk', 'mushrooms', 'onion', 'orange', 'peach', 'peppers', 'potatoes', 'red onion', 'spinach', 'spring onion', 'strawberries', 'sweet_potato', 'tomatoes', 'yoghurt']
 
-
-def show_labels(label_file_path, image_file_path, category_values_list):
+def show_labels(label_file_path, image_file_path, category_values_list, image_number):
     """ This function takes the locations of an image and its corresponding label file and it shows cropped versions of the image that only show the labels.
     label_file_path: (String) This paramater is the pathname of the label file that corresponds to the image.
     image_file_path: (String) This parameter is the pathname of the image file that we will be cropping.
@@ -78,82 +78,91 @@ def show_labels(label_file_path, image_file_path, category_values_list):
 
     im = Image.open(new_image_file_path) # I had to comment out the "r" for read file here because it was giving me errors.
     width, height = im.size
-
-    with open(label_file_path, 'r') as file:
-        lines = file.readlines()
-
-
-    # Here I am deciphering each of the YOLO labels in this one picture and making lists to store their values
+    try: 
+        with open(label_file_path, 'r') as file:
+            lines = file.readlines()
 
 
-    class_id_list = []
-    center_x_list = []
-    center_y_list = []
-    label_width_list = []
-    label_height_list = []
-    left_multiplier_list = []
-    right_multiplier_list = []
-    top_multiplier_list = []
-    bottom_multiplier_list = []
+        # Here I am deciphering each of the YOLO labels in this one picture and making lists to store their values
 
 
-    for line in lines:
-        parts = line.strip().split()  # Split the line into parts
-
-        # These are the characteristics of each of the labels
-        class_id = int(parts[0])
-        center_x = float(parts[1])
-        center_y = float(parts[2])
-        label_width = float(parts[3])
-        label_height = float(parts[4])
-        left_multiplier = center_x - (label_width / 2)
-        right_multiplier = center_x + (label_width / 2)
-        top_multiplier = center_y + (label_height / 2)
-        bottom_multiplier = center_y - (label_height / 2)
-
-    # Now I am adding each of these values to the lists I created
-        class_id_list.append(class_id)
-        center_x_list.append(center_x)
-        center_y_list.append(center_y)
-        label_width_list.append(label_width)
-        label_height_list.append(label_height)
-        left_multiplier_list.append(left_multiplier)
-        right_multiplier_list.append(right_multiplier)
-        top_multiplier_list.append(top_multiplier)
-        bottom_multiplier_list.append(bottom_multiplier)
-        
+        class_id_list = []
+        center_x_list = []
+        center_y_list = []
+        label_width_list = []
+        label_height_list = []
+        left_multiplier_list = []
+        right_multiplier_list = []
+        top_multiplier_list = []
+        bottom_multiplier_list = []
 
 
+        for line in lines:
+            parts = line.strip().split()  # Split the line into parts
+
+            # These are the characteristics of each of the labels
+            class_id = int(parts[0])
+            center_x = float(parts[1])
+            center_y = float(parts[2])
+            label_width = float(parts[3])
+            label_height = float(parts[4])
+            left_multiplier = center_x - (label_width / 2)
+            right_multiplier = center_x + (label_width / 2)
+            top_multiplier = center_y + (label_height / 2)
+            bottom_multiplier = center_y - (label_height / 2)
+
+        # Now I am adding each of these values to the lists I created
+            class_id_list.append(class_id)
+            center_x_list.append(center_x)
+            center_y_list.append(center_y)
+            label_width_list.append(label_width)
+            label_height_list.append(label_height)
+            left_multiplier_list.append(left_multiplier)
+            right_multiplier_list.append(right_multiplier)
+            top_multiplier_list.append(top_multiplier)
+            bottom_multiplier_list.append(bottom_multiplier)
+            
 
 
-        # Process the extracted information 
-        print(f"Class ID: {class_id}, Center X: {center_x}, Center Y: {center_y}, Width: {label_width}, Height: {label_height}")
 
 
-        # I'm not sure why the bottom_bound is bigger than the top_bound, but I think it might be because it's being measured from the top? I might need to rename my variables.
-        # In these lines I am multiplying the Yolo float values which are between 0 and 1 by the width and height of the original picture so that I get the locations of the pixels.
-        print("This is a " + str(category_values_list[class_id]))
-        left_bound = round(left_multiplier * width)
-        print("The left bound is " + str(left_bound))
-        right_bound = round(right_multiplier * width)
-        print("The right bound is " + str(right_bound))
-        top_bound = round(bottom_multiplier * height)
-        print("The top bound is " + str(top_bound))
-        bottom_bound = round(top_multiplier * height)
-        print("The bottom bound is " + str(bottom_bound))
+            # Process the extracted information 
+            print(f"Class ID: {class_id}, Center X: {center_x}, Center Y: {center_y}, Width: {label_width}, Height: {label_height}")
 
 
-        # These lines display the items that are labeled!
-        im1 = im.crop((left_bound, top_bound, right_bound, bottom_bound))
-        im1.show()
-        im1.save("/Users/jacksusank/Downloads/p-ai/fridge_data/Our_Data/" + str(category_values_list[class_id]) + "/image1.jpg")
+            # I'm not sure why the bottom_bound is bigger than the top_bound, but I think it might be because it's being measured from the top? I might need to rename my variables.
+            # In these lines I am multiplying the Yolo float values which are between 0 and 1 by the width and height of the original picture so that I get the locations of the pixels.
+            print("This is a " + str(category_values_list[class_id]))
+            left_bound = round(left_multiplier * width)
+            print("The left bound is " + str(left_bound))
+            right_bound = round(right_multiplier * width)
+            print("The right bound is " + str(right_bound))
+            top_bound = round(bottom_multiplier * height)
+            print("The top bound is " + str(top_bound))
+            bottom_bound = round(top_multiplier * height)
+            print("The bottom bound is " + str(bottom_bound))
 
-show_labels(new_label_file_path, new_image_file_path, names)
+
+            # These lines display the items that are labeled!
+            try:
+                im1 = im.crop((left_bound, top_bound, right_bound, bottom_bound))
+                # im1.show()
+                print("right before save")
+                im1.save("/Users/jacksusank/Downloads/p-ai/fridge_data/Our_Data/" + str(category_values_list[class_id]) + "/" + str(category_values_list[class_id]) + str(image_number) + ".jpg")
+                print("right after save")
+            except: 
+                print(f"The file at '{images_pathname_list[i]}' is not a valid image file.")
+    except: 
+        print("Can't read file lines of " + label_file_path)
+
+
+# show_labels(new_label_file_path, new_image_file_path, names)
 
 
 #Cheese plate folder path
 my_folder_path = "/Users/jacksusank/Downloads/p-ai/fridge_data/SmarterChef.v5i.yolov7pytorch/train"
 
+print("spot 1")
 
 # Define a function you want to apply to each file
 images_pathname_list = []
@@ -161,34 +170,36 @@ labels_pathname_list = []
 
 def make_cropped_pictures_folder(folder_path):
     for subfolder in os.listdir(folder_path):
-        subfolder_path = os.path.join(folder_path, subfolder)
-        if subfolder_path == "/Users/jacksusank/Downloads/p-ai/fridge_data/SmarterChef.v5i.yolov7pytorch/train/images":
-            num = 0
-            for filename in os.listdir(folder_path):
-                num += 1
-                # Construct the full file path
-                images_file_path = os.path.join(folder_path, filename)
-                images_pathname_list.append(images_file_path)
-        elif subfolder_path == "/Users/jacksusank/Downloads/p-ai/fridge_data/SmarterChef.v5i.yolov7pytorch/train/labels":
-            num = 0
-            for filename in os.listdir(folder_path):
-                num += 1
-                # Construct the full file path
-                labels_file_path = os.path.join(folder_path, filename)
-                labels_pathname_list.append(labels_file_path)
-    for i in range(len(10)):
-        try:
-            with Image.open(images_pathname_list[i]) as img:
-                # Image was successfully opened, you can work with it here
-                show_labels(images_pathname_list[i], labels_pathname_list[i], names)
-        except:
-            # Handle the case where the file is not a valid image
+        subfolder_path = (folder_path + "/" + subfolder)
+        print(subfolder)
+        if subfolder != ".DS_Store":
+            if subfolder_path == "/Users/jacksusank/Downloads/p-ai/fridge_data/SmarterChef.v5i.yolov7pytorch/train/images":
+                num = 0
+                for filename in os.listdir(subfolder_path):
+                    num += 1
+                    # Construct the full file path
+                    images_file_path = (subfolder_path + "/" + filename)
+                    print(images_file_path)
+                    images_pathname_list.append(images_file_path)
+            elif subfolder_path == "/Users/jacksusank/Downloads/p-ai/fridge_data/SmarterChef.v5i.yolov7pytorch/train/labels":
+                num = 0
+                for filename in os.listdir(subfolder_path):
+                    num += 1
+                    # Construct the full file path
+                    labels_file_path_and_num = ((subfolder_path + "/" + filename), num)
+                    print(labels_file_path_and_num[0])
+                    labels_pathname_list.append(labels_file_path_and_num)
+            else:
+                raise ("Wrong File!")
+    for i in range(10):
+        #Image.open(images_pathname_list[i]) as img:
+        show_labels(labels_pathname_list[i][0], images_pathname_list[i], names, labels_pathname_list[i][1])
 
-            print(f"The file at '{images_pathname_list[i]}' is not a valid image file.")
+ 
+make_cropped_pictures_folder("/Users/jacksusank/Downloads/p-ai/fridge_data/SmarterChef.v5i.yolov7pytorch/train")
 
 
-
-
+print("spot 2")
 
 
 
@@ -236,7 +247,7 @@ model = model.to(device)
 requires_grad=False
 
 
-
+print("spot 3")
 # Setting up the datasets
 # train_dir = "/Users/jacksusank/Downloads/p-ai/fridge_data/Food_Item_Detection.v1i.yolov7pytorch/train/images"
 # test_dir = "/Users/jacksusank/Downloads/p-ai/fridge_data/Food_Item_Detection.v1i.yolov7pytorch/test/images" 
@@ -268,6 +279,10 @@ test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=12, shuff
 
 
 
+
+
+
+
 # Define loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
@@ -275,6 +290,8 @@ optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
 # Set the random seeds
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
+
+
 
 
 
