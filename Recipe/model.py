751d083b212ea266656ff7bca16887e8cf121eac
Tallie <tmyoo@gmail.com>
2023-11-29 09:40:28 -0800

    kind of working with model

--- Recipe/model.py
+++ Recipe/model.py
@@ -1,17 +1,23 @@
 import torch
 
-model = torch.load('test.pth', map_location=torch.device('cpu'))
+model = torch.load('11-27-23_model_v2.pth', map_location=torch.device('cpu'))
 
 import requests
 from PIL import Image
-from torchvision import transforms
+from torchvision import datasets, models, transforms
 
-#list of labels
-labels = []
+transforms_test = transforms.Compose([
+    transforms.Resize((224, 224)),   #must same as here
+    transforms.CenterCrop((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
 
 def predict(inp):
-  inp = transforms.ToTensor()(inp).unsqueeze(0)
-  with torch.no_grad():
-    prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
-    confidences = {labels[i]: float(prediction[i]) for i in range(1000)}
-  return confidences
+    inp = transforms_test(inp)
+    input = inp.unsqueeze(0)
+    with torch.no_grad():
+        output = model(input)
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+    predicted_class=torch.argmax(probabilities).item()
+    return predicted_class
